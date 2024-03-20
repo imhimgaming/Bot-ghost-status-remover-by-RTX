8466325afbd693892ad0f79e3893778fdaca3798
imhimgaming <164230322+imhimgaming@users.noreply.github.com>
2024-03-20 20:31:48 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = [.gg/kbclan","making kb clan better...."];
 
 
 let currentIndex = 0;
